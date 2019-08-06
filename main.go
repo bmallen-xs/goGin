ed7277eb27a1d819316c7ed8da545b58daa3695b
ben <ben@ascend6.com>
2019-08-06 20:04:26 +0000

    Initial Commit.

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	Port = "8050"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", indexEndpoint)
+
+	port := Port
+
+	if len(os.Getenv("PORT")) > 0 {
+		port = os.Getenv("PORT")
+	}
+	r.Run(":" + port)
+}
+
+func indexEndpoint(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "ok", "code": 200})
+}
